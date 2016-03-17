96aa292ba61eb4b213feacd1651ac43d35ff6b50
Tyler <tyler.rinker@gmail.com>
2016-03-16 21:55:47 -0400

    added as_list to inverse mtabulate

--- R/mtabulate.R
+++ R/mtabulate.R
@@ -1,19 +1,20 @@
 #' Tabulate Frequency Counts for Multiple Vectors
 #'
-#' Similar to \code{\link[base]{tabulate}} that works on multiple vectors.
+#' \code{mtabulate} - Similar to \code{\link[base]{tabulate}} that works on multiple vectors.
 #'
 #' @param vects A \code{\link[base]{vector}}, \code{\link[base]{list}}, or
 #' \code{\link[base]{data.frame}} of named/unnamed vectors.
 #' @keywords tabulate frequency
 #' @export
 #' @seealso \code{\link[base]{tabulate}}, \code{\link[qdapTools]{counts2list}}
-#' @return Returns a \code{\link[base]{data.frame}} with columns equal to
+#' @return \code{mtabulate} - Returns a \code{\link[base]{data.frame}} with columns equal to
 #' number of unique elements and the number of rows equal to the the original
 #' length of the \code{\link[base]{vector}}, \code{\link[base]{list}}, or
 #' \code{\link[base]{data.frame}} (length equals ncols in
 #' \code{\link[base]{data.frame}}).  If list of vectors is named
 #' these will be the rownames of the dataframe.
 #' @author Joran Elias and Tyler Rinker <tyler.rinker@@gmail.com>.
+#' @rdname mtabulate
 #' @references \url{http://stackoverflow.com/a/9961324/1000343}
 #' @examples
 #' mtabulate(list(w=letters[1:10], x=letters[1:5], z=letters))
@@ -25,7 +26,9 @@
 #'
 #' dat <- data.frame(matrix(sample(c("A", "B"), 30, TRUE), ncol=3))
 #' mtabulate(dat)
+#' as_list(mtabulate(dat))
 #' t(mtabulate(dat))
+#' as_list(t(mtabulate(dat)))
 mtabulate <- function(vects) {
     lev <- sort(unique(unlist(vects)))
     dat <- do.call(rbind, lapply(vects, function(x, lev){
@@ -34,3 +37,25 @@ mtabulate <- function(vects) {
     colnames(dat) <- sort(lev)
     data.frame(dat, check.names = FALSE)
 }
+
+
+
+#' Tabulate Frequency Counts for Multiple Vectors
+#'
+#' \code{as_list} - Convert a count matrix to a named list of elements.  The
+#' semantic inverse of \code{mtabulate}.
+#'
+#' @param mat A matrix of counts.
+#' @param nm A character vector of names to assign to the list.
+#' @rdname mtabulate
+#' @return \code{as_list} - Returns a list of elements.
+#' @export
+as_list <- function(mat, nm = rownames(mat)) {
+    nms <- colnames(mat)
+    lst <- apply(mat, 1, function(x) rep(nms, x))
+    if(!is.list(lst)) lst <- lapply(1:ncol(lst), function(i) lst[, i])
+    stats::setNames(lst,  nm = nm)
+}
+
+
+
