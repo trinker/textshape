25fd8a13c7199bb6718170af235a0ef3e0d47605
Tyler <tyler.rinker@gmail.com>
2017-02-07 21:12:10 -0500

    * `tidy_list` picks up the ability to tidy a list of named vectors into three   columns.

--- R/tidy_list.R
+++ R/tidy_list.R
@@ -1,4 +1,4 @@
-#' Tidy a List of Named Dataframes or Vectors
+#' Tidy a List of Named Dataframes or Named Vectors or Vectors
 #'
 #' \code{\link[base]{rbind}} a named \code{\link[base]{list}} of
 #' \code{\link[base]{data.frame}}s or \code{\link[base]{vector}}s to
@@ -9,7 +9,10 @@
 #' \code{\link[base]{data.frame}}s or \code{\link[base]{vector}}.
 #' @param id.name The name to use for the column created from the \code{\link[base]{list}}.
 #' @param content.name The name to use for the column created from the \code{\link[base]{list}}
-#' of \code{\link[base]{vector}}s (only used if \code{x} is  \code{\link[base]{vector}}).
+#' of \code{\link[base]{vector}}s (only used if \code{x} is \code{\link[base]{vector}}).
+#' @param content.attribute.name The name to use for the column created from the
+#' \code{\link[base]{list}} of names given to the \code{\link[base]{vector}}s
+#' (only used if \code{x} is named \code{\link[base]{vector}}).
 #' @param \ldots Ignored.
 #' @return Returns a \code{\link[data.table]{data.table}} with the \code{\link[base]{names}}
 #' from the \code{\link[base]{list}} as an \code{id} column.
@@ -17,6 +20,17 @@
 #' @examples
 #' tidy_list(list(p=1:500, r=letters))
 #' tidy_list(list(p=mtcars, r=mtcars, z=mtcars, d=mtcars))
+#'
+#' x <- list(
+#'     a = setNames(c(1:4), LETTERS[1:4]),
+#'     b = setNames(c(7:9), LETTERS[7:9]),
+#'     c = setNames(c(10:15), LETTERS[10:15]),
+#'     d = c(x=4, y=6, 4),
+#'     e = setNames(1:10, sample(state.abb, 10, TRUE)),
+#'     f = setNames(1:10, sample(month.abb, 10, TRUE))
+#' )
+#'
+#' tidy_list(x)
 #'
 #' \dontrun{
 #' ## 2015 Vice-Presidential Debates Example
@@ -41,7 +55,8 @@
 #' }) %>%
 #'     textshape::tidy_list("location")
 #' }
-tidy_list <- function(x, id.name= "id", content.name = "content", ...){
+tidy_list <- function(x, id.name= "id", content.name = "content",
+    content.attribute.name = 'attribute', ...){
 
     if (is.data.frame(x[[1]])){
         tidy_list_df(x = x, id.name = id.name)
@@ -68,19 +83,30 @@ tidy_list_df <- function (x, id.name = "id"){
     data.table::data.table(out)
 }
 
-tidy_list_vector <- function(x, id.name= "id", content.name = "content"){
+tidy_list_vector <- function(x, id.name = "id",
+    content.attribute.name = 'attribute', content.name = "content"){
+
     if (is.null(names(x))) {
         names(x) <- seq_along(x)
     }
-    dat <- data.frame(
-        rep(names(x), sapply(x, length)),
-        unlist(x, use.names = FALSE),
-        stringsAsFactors = FALSE,
-        check.names = FALSE,
-        row.names = NULL
-    )
-    colnames(dat) <- c(id.name, content.name)
-    data.table::data.table(dat)
+
+    if (all(!sapply(x, function(y) is.null(names(y))))){
+
+        tidy_list(lapply(x, tidy_vector, content.attribute.name , content.name ), id.name)
+
+    } else {
+
+        dat <- data.frame(
+            rep(names(x), sapply(x, length)),
+            unlist(x, use.names = FALSE),
+            stringsAsFactors = FALSE,
+            check.names = FALSE,
+            row.names = NULL
+        )
+        colnames(dat) <- c(id.name, content.name)
+        data.table::data.table(dat)
+
+    }
 }
 
 
