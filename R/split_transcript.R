9d97f6149f8756bc1b19e544d5f2edc42a80a805
Tyler <tyler.rinker@gmail.com>
2017-05-24 21:20:33 -0400

    up

--- R/split_transcript.R
+++ R/split_transcript.R
@@ -6,7 +6,11 @@
 #'
 #' @param x A transcript style vector (e.g., \code{c("greg: Who me", "sarah: yes you!")}.
 #' @param delim The delimiter to split on.
-#' @param colnames The column names to use for the \code{\link[data.table]{data.table}} output.
+#' @param colnames The column names to use for the \code{\link[data.table]{data.table}}
+#' output.
+#' @param max.delim An integer stating how many characters may come before a
+#' delimiter is found.  This is useful for the case when a colon is the delimiter
+#' but time stamps are also found in the text.
 #' @param \ldots Ignored.
 #' @return Returns a 2 column \code{\link[data.table]{data.table}}.
 #' @export
@@ -36,10 +40,14 @@
 #' })
 #' }
 split_transcript <- function(x, delim = ":", colnames = c("person", "dialogue"),
-    ...){
+    max.delim = 15, ...){
 
     V1 <- V2 <- NULL
-    x <- sub(delim, "textshapesplithere", x)
+    if (!is.null(max.delim)) {
+        x <- gsub(paste0('(^[^',delim, ']{0,', max.delim, '})([', delim, '])(.*$)'), "\\1textshapesplithere\\3", x, perl = TRUE)
+    } else {
+        x <- sub(delim, "textshapesplithere", x)
+    }
     dat <- data.table::data.table(do.call(rbind, strsplit(x , "textshapesplithere")))[,
         V1 := trimws(V1)][,
         V2 := trimws(V2)][]
