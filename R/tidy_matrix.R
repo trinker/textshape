8e7c9f181e8484359c778574d35ae09790ba08b5
Tyler Rinker <trinker@campuslabs.com>
2017-08-22 12:51:00 -0400

    swapped adjaceny for adjacency

--- R/tidy_matrix.R
+++ R/tidy_matrix.R
@@ -16,8 +16,8 @@
 #' tidy_matrix(as.matrix(simple_dtm), 'doc', 'term', 'n')
 #' 
 #' X <- as.matrix(simple_dtm[1:10, 1:10])
-#' tidy_adjaceny_matrix(crossprod(X))
-#' tidy_adjaceny_matrix(crossprod(t(X)))
+#' tidy_adjacency_matrix(crossprod(X))
+#' tidy_adjacency_matrix(crossprod(t(X)))
 tidy_matrix <- function(x, row.name = 'row', col.name = 'col', value.name = 'value', ...){
     
     stopifnot(is.matrix(x))
@@ -33,13 +33,13 @@ tidy_matrix <- function(x, row.name = 'row', col.name = 'col', value.name = 'val
 
 #' Convert a Matrix into Tidy Form
 #' 
-#' \code{tidy_adjaceny_matrix} - A wrapper for \code{tidy_matrix} with the 
+#' \code{tidy_adjacency_matrix} - A wrapper for \code{tidy_matrix} with the 
 #' \code{row.name}, \code{col.name}, & \code{value.name} all set to \code{"from"},
 #' \code{"to"}, & \code{"n"}, assuming peparation for network analysis.
 #' 
 #' @export
 #' @rdname tidy_matrix
-tidy_adjaceny_matrix <- function(x, ...){
+tidy_adjacency_matrix <- function(x, ...){
     
     tidy_matrix(x, row.name = 'from', col.name = 'to', value.name = 'n', ...)
     
