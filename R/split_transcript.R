b94264bada5ff18b3e19d84cec387f910f727edf
Tyler Rinker <trinker@campuslabs.com>
2018-11-02 23:06:24 -0400

    * `split_transcript` now has more accurate regex group capturing.

--- R/split_transcript.R
+++ R/split_transcript.R
@@ -42,12 +42,11 @@
 split_transcript <- function(x, delim = ":", colnames = c("person", "dialogue"),
     max.delim = 15, ...){
 
-    V1 <- V2 <- NULL
     
     if (!is.null(max.delim)) {
         x <- gsub(
-            paste0('(^[^',delim, ']{0,', max.delim, '})([', delim, '])(.*$)'), 
-            "\\1textshapesplithere\\3", 
+            paste0('(^[^',delim, ']{0,', max.delim, '})([', delim, '])'), 
+            "\\1textshapesplithere", 
             x, 
             perl = TRUE
         )
@@ -58,8 +57,8 @@ split_transcript <- function(x, delim = ":", colnames = c("person", "dialogue"),
     dat <- data.table::data.table(
             do.call(rbind, strsplit(x , "textshapesplithere"))
         )[,
-        V1 := trimws(V1)][,
-        V2 := trimws(V2)][]
+        'V1' := trimws(V1)][,
+        'V2' := trimws(V2)][]
     
     data.table::setnames(dat, c("V1", "V2"), colnames)
     dat
